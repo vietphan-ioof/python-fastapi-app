535d16985f46c47c62f696f620dbdadcfb24f4a4
Viet Phan <109930734+vietphan-ioof@users.noreply.github.com>
2022-11-25 13:39:52 +1100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
+def read_item(item_id: int, q: str=None):
     return {"item_id": item_id, "q": q}
