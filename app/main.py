dec8a7caea32d4fe955c8406f96faaf44bbc9554
Viet Phan <109930734+vietphan-ioof@users.noreply.github.com>
2022-11-30 00:33:19 +1100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/ping")
+def read_root():
+    return {"status": "pong"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str=None):
     return {"item_id": item_id, "q": q}
